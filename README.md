6bc2ef9ce41539055a7a11b445dc1301512e53a1
debrajsa <debrajsa@mindspring.com>
2021-12-26 05:19:49 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-1e7x51
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-1e7x51)
---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { useState } from 'react';
+import './style.css';
+import Form from './components/Form';
+import CardList from './components/CardList';
+
+export default function App() {
+  const [cards, setCards] = useState([]);
+  const addNewCard = (card) => {
+    setCards([...cards, card]);
+  };
+  return (
+    <div>
+      <h1 className="pt-10 text-center mt-6 text-3xl leading-9 font-extrabold text-gray-900">
+        Search a GitHub User
+      </h1>
+      <Form onSubmit={addNewCard} />
+      <CardList cards={cards} />
+    </div>
+  );
+}
